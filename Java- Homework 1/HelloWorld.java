4d68e72e8c5baa5b186618d181465f3e211ca2d6
Hoangnguyenhao <tranhoangnguyenhao2003@gmail.com>
2025-07-23 19:28:11 +0700

    Add files via upload

---
+++ Java- Homework 1/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    private static void printMessage() {
+        System.out.println("Hello world");
+    }
+}
